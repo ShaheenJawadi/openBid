a534055f32ce056769bd88ab22a2788617df4de2
shaheenj <jawadishaheen@gmail.com>
2024-12-03 16:54:33 +0100

    Configure Spring Boot application properties

--- src/main/java/com/example/openbid/OpenBidApplication.java
+++ src/main/java/com/example/openbid/OpenBidApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OpenBidApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(OpenBidApplication.class, args);
+
+
     }
 
 }
