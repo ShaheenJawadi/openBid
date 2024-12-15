4ea82e4278165a325b90f3e9dc8b68df67c823bb
shaheenj <jawadishaheen@gmail.com>
2024-12-15 10:06:32 +0100

    Update Item Repository to include a custom query method  for finding items by seller ID

--- src/main/java/com/example/openbid/repositories/ItemRepository.java
+++ src/main/java/com/example/openbid/repositories/ItemRepository.java
@@ -3,5 +3,8 @@ package com.example.openbid.repositories;
 import com.example.openbid.model.Item;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends CrudRepository<Item,Long> {
+    List<Item> findBySellerId(Long sellerId);
 }
