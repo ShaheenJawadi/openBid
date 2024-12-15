3cabf0c6599f96e94996b7e781d82e2303551d4a
shaheenj <jawadishaheen@gmail.com>
2024-12-15 10:16:27 +0100

    Update Auction Repository to include a custom query method  for finding auction by item ID

--- src/main/java/com/example/openbid/repositories/AuctionRepository.java
+++ src/main/java/com/example/openbid/repositories/AuctionRepository.java
@@ -3,5 +3,8 @@ package com.example.openbid.repositories;
 import com.example.openbid.model.Auction;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface AuctionRepository extends CrudRepository<Auction,Long> {
+    Optional<Auction> findByItemId(Long itemId);
 }
