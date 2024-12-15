7cc5e3fbfa16b9c2c6105eba9f16039d992a41b2
shaheenj <jawadishaheen@gmail.com>
2024-12-15 09:57:45 +0100

    Implement UserService class with UserServiceInterface

--- src/main/java/com/example/openbid/services/UserService.java
+++ src/main/java/com/example/openbid/services/UserService.java
@@ -2,9 +2,14 @@ package com.example.openbid.services;
 
 
 import com.example.openbid.model.User;
+import com.example.openbid.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 interface UserServiceInterface {
     User registerUser(User user);
@@ -12,6 +17,25 @@ interface UserServiceInterface {
     List<User> getAllUsers();
 }
 
+@Service
+public class UserService implements UserServiceInterface {
+    @Autowired
+    private UserRepository userRepository;
 
-public class UserService {
+    @Override
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return StreamSupport.stream(userRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
 }
