8dd1431b107c9c4345480fe2702d4b9afb415dff
shaheenj <jawadishaheen@gmail.com>
2024-12-15 10:10:33 +0100

    Update Bid Repository to include a custom query method  for finding bid by item ID

--- src/main/java/com/example/openbid/repositories/BidRepository.java
+++ src/main/java/com/example/openbid/repositories/BidRepository.java
@@ -3,5 +3,8 @@ package com.example.openbid.repositories;
 import com.example.openbid.model.Bid;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BidRepository extends CrudRepository<Bid,Long> {
+    List<Bid> findByItemId(Long itemId);
 }
