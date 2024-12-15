fe8a8680c5e0c061539c68647a664e157c2998f3
shaheenj <jawadishaheen@gmail.com>
2024-12-15 10:07:44 +0100

    Implement ItemService class with ItemServiceInterface

--- src/main/java/com/example/openbid/services/ItemService.java
+++ src/main/java/com/example/openbid/services/ItemService.java
@@ -2,6 +2,10 @@ package com.example.openbid.services;
 
 
 import com.example.openbid.model.Item;
+import com.example.openbid.repositories.ItemRepository;
+import com.example.openbid.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,5 +17,36 @@ interface ItemServiceInterface {
     List<Item> getItemsBySeller(Long sellerId);
 }
 
-public class ItemService {
+
+@Service
+public class ItemService  implements ItemServiceInterface {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Item createItem(Item item) {
+        userRepository.findById(item.getSeller().getId())
+                .orElseThrow(() -> new RuntimeException("Seller not found"));
+
+        return itemRepository.save(item);
+    }
+
+    @Override
+    public Optional<Item> getItemById(Long id) {
+        return itemRepository.findById(id);
+    }
+
+    @Override
+    public List<Item> getAllItems() {
+        return (List<Item>) itemRepository.findAll();
+    }
+
+    @Override
+    public List<Item> getItemsBySeller(Long sellerId) {
+        return itemRepository.findBySellerId(sellerId);
+    }
 }
