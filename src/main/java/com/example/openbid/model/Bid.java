0f988914d49e6534a99a8d7a1d60ba0dd93369b2
shaheenj <jawadishaheen@gmail.com>
2024-12-15 08:48:39 +0100

    Add Bid Entity

---
+++ src/main/java/com/example/openbid/model/Bid.java
@@ -0,0 +1,34 @@
+package com.example.openbid.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+
+@Entity
+@Table(name = "bids")
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Bid {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bidder_id", nullable = false)
+    private User bidder;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private double amount;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @org.hibernate.annotations.CreationTimestamp
+    private Date bidTime;
+}
