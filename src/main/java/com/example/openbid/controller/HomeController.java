131331bdf307c1f4ed06229581d890d15b1c9f59
shaheenj <jawadishaheen@gmail.com>
2024-12-18 07:27:21 +0100

    Setup  single item page

--- src/main/java/com/example/openbid/controller/HomeController.java
+++ src/main/java/com/example/openbid/controller/HomeController.java
@@ -1,14 +1,25 @@
 package com.example.openbid.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "publicPages/index";
     }
 
+
+    @GetMapping("/item/{slug}")
+    public String item(@PathVariable("slug") String slug, Model model) {
+        //Item item = itemService.getItemBySlug(slug)
+        model.addAttribute("itemSlug", slug);
+
+            return "publicPages/itemPage";
+
+    }
 }
