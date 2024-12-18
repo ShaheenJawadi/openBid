9d5bcb231e61067d7a3572ffd6c81e11e63548c1
shaheenj <jawadishaheen@gmail.com>
2024-12-18 09:53:44 +0100

    Add login and signup request handler with validator

---
+++ src/main/java/com/example/openbid/dto/request/LoginRequest.java
@@ -0,0 +1,26 @@
+package com.example.openbid.dto.request;
+import jakarta.validation.constraints.*;
+
+public class LoginRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
